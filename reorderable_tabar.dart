be7f81a0d4bdf8da3602aeaf1422922dbf20f627
dotcubetitan7 <dotcubetitan7@gmail.com>
2025-09-20 18:07:29 +0530

    Update reorderable_tabar.dart

--- reorderable_tabar.dart
+++ reorderable_tabar.dart
@@ -1,1 +1,33 @@
 
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TabView Tutorial GFG',
+      theme: ThemeData(
+        
+        primarySwatch: Colors.green,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+class HomePage extends StatelessWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: length, 
+      child: child,      
+    );
+  }
+}
